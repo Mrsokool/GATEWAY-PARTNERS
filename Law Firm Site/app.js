5e957d8f05fa796cba5f3992e7b2c5d0561f3025
Mgbejinaka Ifeanyi Emmanuel <mistersokool@gmail.com>
2022-06-12 10:20:43 +0100

    Add files via upload
    Working on making this site responsive

---
+++ Law Firm Site/app.js
@@ -0,0 +1,9 @@
+function navlinks() {
+    var x = document.getElementsByClassName("links");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
